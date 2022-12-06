d9deab6d2f5b68da50cd28b64efd76be6a9868e8
amir <amir.salimi1810@gmail.com>
2022-12-06 17:14:11 +0100

    first

---
+++ scripts/responsive.js
@@ -0,0 +1,5 @@
+let main = document.querySelector(".main");
+let back = document.querySelector(".back");
+
+main.style["grid-template-columns"] = "2fr 1fr";
+back.style["display"] = "grid";
---
+++ scripts/timeLocation.js
@@ -0,0 +1,24 @@
+const time = new Date();
+const hour = time.getHours();
+let dayWeek = document.querySelector(".dayWeek");
+const date = time.getDate();
+const dateHTML = document.querySelector(".day");
+const monthHTML = document.querySelector(".month");
+let backGround = document.querySelector(".backGround");
+
+let dayNight = "night";
+
+if (hour > 5 && hour < 18) {
+  dayNight = "day";
+}
+
+//give the name of day like monday, tuesday and...
+const dayName = time.toLocaleDateString("us-en", { weekday: "long" });
+const monthName = time.toLocaleString("us-en", { month: "long" });
+dayWeek.innerHTML = dayName;
+
+//month
+dateHTML.innerHTML = date + ".";
+monthHTML.innerHTML = monthName;
+
+backGround.style["background-image"] = 'url("background/day.png")';
